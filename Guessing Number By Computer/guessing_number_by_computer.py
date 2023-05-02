ae54d00978ff7376e14859f329685508a501ad87
bavaaram <itsjustabavaar@gmail.com>
2023-05-03 02:01:02 +0330

    Add some feathures like Correct botton (c) and handling other statemants.

--- Guessing Number By Computer/guessing_number_by_computer.py
+++ Guessing Number By Computer/guessing_number_by_computer.py
@@ -1,4 +1,7 @@
-#! /usr/bin/python3
+#! /usr/bin/python
+"""
+This Program is for Guessing the number that user entered by computer.
+"""
 
 import random
 
@@ -8,7 +11,7 @@ def guess_by_pc():
     This function is a game. You Enter a Number betwen
     1 & 100 and computer must guess it!
     """
-    a, b = 1, 100
+    low, high = 1, 100
     while 1:
         try:
             x_1 = int(input("Please Enter a number between 1 & 100: "))
@@ -17,22 +20,24 @@ def guess_by_pc():
             continue
         else:
             break
-    guess = random.randint(a, b)
-    while guess != x_1:
-        print(f"The Guessed number is {guess}. status? l/h", end="\t")
+    guess = random.randint(low, high)
+    while 1:
         while 1:
+            print(f"The Guessed number is {guess}. status? l/h/c", end="\t")
             stat = input()
-            if stat not in ("l", "h"):
+            if stat not in ("l", "h", "c"):
                 print("Invalid Input.")
             else:
                 break
         if stat == "l":
-            a = guess + 1
-            guess = random.randint(a, b)
+            low = guess + 1
+            guess = random.randint(low, high)
+        elif stat == "h":
+            high = guess - 1
+            guess = random.randint(low, high)
         else:
-            b = guess - 1
-            guess = random.randint(a, b)
-    print(f"The Guessed number is {guess}.\n Correct!")
+            print(f"Correct! the number is {x_1}")
+            break
 
 
 guess_by_pc()
